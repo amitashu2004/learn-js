7729de9d2a7e25e1ca4ed39d65743c7c3cf9b647
amitashu2004 <amitashu2004@gmail.com>
2025-08-27 16:56:24 +0000

    Boolean

--- variables.js
+++ variables.js
@@ -1,2 +1,16 @@
 console.log(" I will finish JS this week");
 console.log(" I will finish JS this weekend");
+let big = 123456789012345678901234567890n;
+console.log(big + 1n); 
+
+let scope = null;
+console.log(typeof scope);
+
+//let numScope = Number(scope);
+//let typeOfScope = (typeof numScope);
+//console.log(typeOfScope);
+
+let boolScope = Boolean(scope);
+let typeOfboolScope = (typeof boolScope);
+console.log(boolScope);
+console.log(typeOfboolScope);
